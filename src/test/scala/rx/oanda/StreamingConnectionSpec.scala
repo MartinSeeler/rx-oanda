0348ed3939d56c911c9a0d9cb1a8ace41618ea6a
Martin Seeler <mseeler@avantgarde-labs.de>
2016-01-31 18:01:30 +0100

    Add spec for Streaming connection

--- src/test/scala/rx/oanda/StreamingConnectionSpec.scala
+++ src/test/scala/rx/oanda/StreamingConnectionSpec.scala
@@ -27,9 +27,12 @@ import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 import akka.stream.testkit.javadsl.TestSink
 import akka.util.ByteString
+import cats.data.Xor
 import org.scalatest._
 import rx.oanda.accounts.SandboxAccount
 import rx.oanda.errors.{InvalidInstrument, OandaException}
+import rx.oanda.rates.Price
+import rx.oanda.utils.Heartbeat
 
 import scala.concurrent.Future
 
@@ -41,15 +44,15 @@ class StreamingConnectionSpec extends FlatSpec with Matchers with Scalatest {
   implicit val mat = ActorMaterializer()
   import system.dispatcher
 
-  val serverSource = Http().bind("localhost", 8080)
+  val serverSource = Http().bind("localhost", 8081)
 
   val requestHandler: HttpRequest => Future[HttpResponse] = {
-    case HttpRequest(GET, Uri.Path("/noGzip"), _, _, _) =>
-      Future.successful(HttpResponse(entity = HttpEntity(ContentTypes.`application/json`,
-        "{\"username\":\"keith\",\"password\":\"Rocir~olf4\",\"accountId\":8954947}")))
-    case HttpRequest(GET, Uri.Path("/sandboxAccount"), _, _, _) =>
-      Future.successful(HttpResponse(entity = HttpEntity(ContentTypes.`application/json`,
-        data = Source.single(ByteString.fromString("{\"username\":\"keith\",\"password\":\"Rocir~olf4\",\"accountId\":8954947}")).via(Gzip.encoderFlow))))
+    case HttpRequest(GET, Uri.Path("/prices"), _, _, _) =>
+      Future.successful(HttpResponse(entity = HttpEntity(contentType = ContentTypes.`application/json`,
+        data = Source(Vector(
+          "{\"tick\":{\"instrument\":\"EUR_USD\",\"time\":\"1453847424597195\",\"bid\":1.08646,\"ask\":1.08668}}",
+          "{\"heartbeat\":{\"time\":\"1453849454039260\"}}"
+        )).map(x ⇒ ByteString.fromString(x)))))
     case HttpRequest(GET, Uri.Path("/oandaError"), _, _, _) =>
       Future.successful(HttpResponse(status = BadRequest,
         entity = HttpEntity(ContentTypes.`application/json`,
@@ -59,38 +62,33 @@ class StreamingConnectionSpec extends FlatSpec with Matchers with Scalatest {
   val bindingFuture: Future[Http.ServerBinding] =
     serverSource.to(Sink.foreach(_ handleWithAsyncHandler requestHandler)).run()
 
-  val apiConnection = new ApiConnection {
-    private[oanda] val apiConnection = Http().cachedHostConnectionPool[Long]("localhost", 8080)
-  }
-
-  it must "fail when the content is not gzip encoded" in {
-    apiConnection
-      .makeRequest[Long](HttpRequest(GET, "/noGzip"))
-      .runWith(TestSink.probe(system))
-      .request(1)
-      .expectError()
+  val streamingConnection = new StreamingConnection {
+    private[oanda] val streamingConnection = Http().cachedHostConnectionPool[Long]("localhost", 8081)
   }
 
   it must "parse an oanda error when the status code is not 200" in {
-    apiConnection
-      .makeRequest[Long](HttpRequest(GET, "/oandaError"))
+    streamingConnection
+      .startStreaming[Long](HttpRequest(GET, "/oandaError"), "anything")
       .runWith(TestSink.probe(system))
       .request(1)
       .expectError(new OandaException(InvalidInstrument("Invalid instrument: EUR_USD%2CEUR_GBP is not a valid instrument")))
   }
 
-  it must "parse a sandbox account with gzip encoding" in {
-    apiConnection
-      .makeRequest[SandboxAccount](HttpRequest(GET, "/sandboxAccount"))
+  it must "parse a wrapped price and heartbeat in the correct order" in {
+    streamingConnection
+      .startStreaming[Price](HttpRequest(GET, "/prices"), "tick")
       .runWith(TestSink.probe(system))
       .request(1)
-      .expectNext(SandboxAccount("keith", "Rocir~olf4", 8954947L))
+      .expectNext(Xor.Left(Price("EUR_USD", 1453847424597195L, 1.08646, 1.08668)))
+      .request(1)
+      .expectNext(Xor.Right(Heartbeat(1453849454039260L)))
+      .expectComplete()
   }
 
   it must "fail when no connection is possible" in {
-    new ApiConnection {
-      private[oanda] val apiConnection = Http().cachedHostConnectionPool[Long]("_", 8080)
-    }.makeRequest[SandboxAccount](HttpRequest(GET, "/sandboxAccount"))
+    new StreamingConnection {
+      private[oanda] val streamingConnection = Http().cachedHostConnectionPool[Long]("_", 8081)
+    }.startStreaming[Price](HttpRequest(GET, "/prices"), "tick")
       .runWith(TestSink.probe(system))
       .request(1)
       .expectError()
